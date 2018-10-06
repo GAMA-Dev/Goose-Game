dc6c9599192e10443787835ab15d8fba2281c8b2
PhoShizz0 <43829396+PhoShizz0@users.noreply.github.com>
2018-10-06 16:08:16 -0400

    Add files via upload

---
+++ Assets/Scripts/Goose_Generator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script generates a number 1-4 to choose what player will be the goose
+
+public class Goose_Generator : MonoBehaviour
+{
+    //variable to hold who is the goose
+    public int Goose;
+
+    void Start()
+    {
+        GooseGen();
+    }
+
+    void GooseGen()
+    {
+        Goose = Random.Range(1, 5);
+    }
+}
